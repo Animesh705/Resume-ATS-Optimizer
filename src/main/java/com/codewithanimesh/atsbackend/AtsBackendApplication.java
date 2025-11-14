acd44b91efac28e639c19f04a4a49316773744e7
Animesh Anand <animeshanand705@gmail.com>
2025-11-14 15:53:36 +0530

    chore: initial Spring Boot project setup

--- src/main/java/com/codewithanimesh/atsbackend/AtsBackendApplication.java
+++ src/main/java/com/codewithanimesh/atsbackend/AtsBackendApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class AtsBackendApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(AtsBackendApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(AtsBackendApplication.class, args);
+    }
 
 }
